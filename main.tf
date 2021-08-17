3e7a6ef98a9a1380bf5925bbbbe4a5ac4f1ce3fa
saim22r <saimrafiq224@gmail.com>
2021-08-17 12:26:51 +0100

    update

--- main.tf
+++ main.tf
@@ -52,9 +52,69 @@ resource "aws_route_table_association" "saim_public_subnet"{
     route_table_id = aws_route_table.saim_RT.id
 }
 
+# Add Network ACL rules
+resource "aws_network_acl" "saim_NACL" {
+   vpc_id = aws_vpc.terraform_vpc.id
+   subnet_ids = [aws_subnet.saim_public_subnet.id]
+
+  ingress {
+    protocol = "tcp"
+    rule_no = 100
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 80
+    to_port = 80
+  }
+
+  ingress {
+    protocol = "tcp"
+    rule_no = 110
+    action = "allow"
+    cidr_block = var.my_ip
+    from_port = 22
+    to_port = 22
+  }
+
+  ingress {
+    protocol = "tcp"
+    rule_no = 120
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 1024
+    to_port = 65535
+  }
+
+  egress {
+    protocol = "tcp"
+    rule_no = 100
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 80
+    to_port = 80
+  }
+
+  egress {
+    protocol = "tcp"
+    rule_no = 110
+    action = "allow"
+    cidr_block = "10.203.2.0/24"
+    from_port = 27017
+    to_port = 27017
+  }
+
+  egress {
+    protocol = "tcp"
+    rule_no = 120
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 80
+    to_port = 80
+  }
+}
+
 # Add Security group rules
-resource "aws_security_group" "ssh" {
-  vpc_id = aws_vpc.prod-vpc.id
+resource "aws_security_group" "saim_SG" {
+  vpc_id = aws_vpc.terraform_vpc.id
 
   egress {
     from_port   = 0
@@ -62,10 +122,11 @@ resource "aws_security_group" "ssh" {
     protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = [var.my_ip]
   }
 
@@ -75,6 +136,14 @@ resource "aws_security_group" "ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "eng89_saim_sg_app"
   }
@@ -86,6 +155,7 @@ resource "aws_instance" "app_instance" {
   subnet_id = aws_subnet.saim_public_subnet.id
   instance_type = "t2.micro"
   associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.saim_SG.id]
   key_name = var.aws_key_name
 
 tags = {
